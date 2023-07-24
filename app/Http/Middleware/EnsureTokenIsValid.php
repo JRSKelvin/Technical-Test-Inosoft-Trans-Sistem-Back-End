62bfaed8df0755fe935fec52126d15278ad4ad71
Kelvin <74870436+JRSKelvin@users.noreply.github.com>
2023-07-24 22:38:35 +0700

    Added Middleware Logic And MongoDB User Authentication From Illuminate User Authentication

--- app/Http/Middleware/EnsureTokenIsValid.php
+++ app/Http/Middleware/EnsureTokenIsValid.php
@@ -3,7 +3,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class EnsureTokenIsValid
 {
@@ -16,6 +18,18 @@ class EnsureTokenIsValid
      */
     public function handle(Request $request, Closure $next)
     {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+            if (!$user) {
+                throw new Exception();
+            }
+        } catch (Exception $ex) {
+            $result = [
+                'status' => 500,
+                'error' => $ex->getMessage(),
+            ];
+            return response()->json($result, $result['status']);
+        }
         return $next($request);
     }
 }
